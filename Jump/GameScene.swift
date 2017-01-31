790d9c8c9ca3dfc082cecee33898f1336da23c9e
Jeff Eom <jeffeom@gmail.com>
2017-01-30 23:40:30 -0800

    fixed git ignore + jump label + fixed jumps

--- Jump/GameScene.swift
+++ Jump/GameScene.swift
@@ -14,16 +14,17 @@ class GameScene: SKScene {
     var start = Bool(false)
     var onFloor1 = Bool(false)
     var onFloor2 = Bool(false)
-    var firstJump = Bool(true)
     var myBackground = SKSpriteNode()
     var myFloor1 = SKSpriteNode()
     var myFloor2 = SKSpriteNode()
     let birdAtlas = SKTextureAtlas(named: "player.atlas")
     var birdSprites = Array<SKTexture>()
     var bird = SKSpriteNode()
-    var jumpSpace = CGFloat(100)
+    var myLabel = SKLabelNode()
+    var jumpLength = CGFloat(50)
+    var holeLength = CGFloat(50)
     var floorLength = CGFloat(100)
-    var birdJumpLength = CGFloat(50)
+    var jumpNumber = 5
     
     override func didMove(to view: SKView) {
         self.backgroundColor = SKColor(red: 80.0/255.0, green: 192.0/255.0, blue: 203.0/255.0, alpha: 1.0)
@@ -37,6 +38,13 @@ class GameScene: SKScene {
         myBackground.position = CGPoint.init(x: 0, y: 0)
         addChild(myBackground)
         
+        myLabel = SKLabelNode.init(fontNamed: "Chalkduster")
+        myLabel.text = String("Next Jump: \(jumpNumber)")
+        myLabel.fontSize = 45
+        myLabel.fontColor = SKColor.black
+        myLabel.position = CGPoint.init(x: frame.midX , y: frame.midY * 1.5)
+        addChild(myLabel)
+        
         myFloor1 = SKSpriteNode(imageNamed: "floor")
         myFloor2 = SKSpriteNode(imageNamed: "floor")
         floorLength = myFloor1.size.width / 1.5
@@ -45,7 +53,7 @@ class GameScene: SKScene {
         myFloor1.anchorPoint = CGPoint.zero
         myFloor1.position = CGPoint.init(x: 0, y: 0)
         myFloor2.anchorPoint = CGPoint.zero
-        myFloor2.position = CGPoint.init(x: myFloor1.size.width, y: 10)
+        myFloor2.position = CGPoint.init(x: myFloor1.size.width, y: myFloor1.position.y)
         addChild(self.myFloor1)
         addChild(self.myFloor2)
         
@@ -69,10 +77,7 @@ class GameScene: SKScene {
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         start = true
         
-        bird.position.x = bird.position.x + birdJumpLength
-        
-        NSLog("\(birdJumpLength) FJ?: \(firstJump)")
-        firstJump = false
+        bird.position.x = bird.position.x + jumpLength
     }
     
     override func update(_ currentTime: TimeInterval) {
@@ -81,96 +86,81 @@ class GameScene: SKScene {
         myFloor2.position = CGPoint.init(x: myFloor2.position.x - 4, y: myFloor2.position.y)
         
         if (!start){
+            // floor 1 is moved out create new floor 1
             if (myFloor1.position.x < -myFloor1.size.width){
                 myFloor1.position = CGPoint.init(x: myFloor2.position.x + myFloor2.size.width, y: myFloor1.position.y)
-                
-                NSLog("boom 1 \(floorLength)")
             }
             
+            // floor 2 is moved out create new floor 2
             if (myFloor2.position.x < -myFloor2.size.width){
                 myFloor2.position = CGPoint.init(x: myFloor1.position.x + myFloor1.size.width, y: myFloor2.position.y)
-                NSLog("boom 2")
             }
         }
         
         if (start) {
             bird.position = CGPoint.init(x: bird.position.x - 4, y: bird.position.y)
             
-            if (myFloor1.position.x < -myFloor1.size.width){
-                jumpSpace = randomBetweenNumbers(firstNum: 50, secondNum: 200)
-                myFloor1.position = CGPoint.init(x: myFloor2.position.x + myFloor2.size.width + jumpSpace, y: myFloor1.position.y)
+            // if floor 1 is moved out of the screen create new floor 1
+            if (myFloor1.position.x < -myFloor1.size.width - 20){
+                jumpNumber = Int(randomBetweenNumbers(firstNum: 5, secondNum: 10))
                 
-                floorLength = randomBetweenNumbers(firstNum: 200, secondNum: 500)
-                NSLog("FloorLength: \(floorLength), JumpSpace: \(jumpSpace)")
+                myLabel.text = String("Next Jump: \(jumpNumber)")
+                
+                holeLength = randomBetweenNumbers(firstNum: 80, secondNum: 120)
+                
+                myFloor1.position = CGPoint.init(x: myFloor2.position.x + myFloor2.size.width + holeLength, y: myFloor1.position.y)
+                
+                floorLength = bird.size.width * CGFloat(jumpNumber)
+                NSLog("FloorLength: \(floorLength), JumpNumber: \(jumpNumber)")
+                
+                jumpLength = floorLength / CGFloat(jumpNumber)
                 
                 myFloor1.size.width = floorLength
-                
             }
             
+            // if floor 2 is moved out of the screen create new floor 2
+            if (myFloor2.position.x < -myFloor2.size.width - 20){
+                jumpNumber = Int(randomBetweenNumbers(firstNum: 5, secondNum: 10))
+                
+                myLabel.text = String("Next Jump: \(jumpNumber)")
+                
+                holeLength = randomBetweenNumbers(firstNum: 80, secondNum: 120)
+                
+                myFloor2.position = CGPoint.init(x: myFloor1.position.x + myFloor1.size.width + holeLength, y: myFloor2.position.y)
+                
+                floorLength = bird.size.width * CGFloat(jumpNumber)
+                NSLog("FloorLength: \(floorLength), JumpNumber: \(jumpNumber)")
+                
+                jumpLength = floorLength / CGFloat(jumpNumber)
+                
+                myFloor2.size.width = floorLength
+            }
+            
+            // if the bird is on the first floor
             if (myFloor1.position.x < bird.position.x && bird.position.x < myFloor1.position.x + myFloor1.size.width){
                 
                 onFloor1 = true
                 onFloor2 = false
                 
-                if firstJump{
-                    birdJumpLength = myFloor1.size.width / 5 - bird.size.width / 2
-                }else{
-                    birdJumpLength = myFloor1.size.width / 5
-                }
-                
+                // if the bird is on the second floor
             }else if (myFloor2.position.x < bird.position.x && bird.position.x < myFloor2.position.x + myFloor2.size.width){
                 
                 onFloor1 = false
                 onFloor2 = true
-                
-                if firstJump{
-                    birdJumpLength = myFloor2.size.width / 5 - bird.size.width / 2
-                }else{
-                    birdJumpLength = myFloor2.size.width / 5
-                }
             }
             
-            if (myFloor1.position.x + myFloor1.size.width < bird.position.x + bird.size.width / 2 && bird.position.x - bird.size.width / 2 < myFloor1.position.x + myFloor1.size.width + myFloor1.size.width / 5 ){
+            // within the death area after floor 1
+            if (myFloor1.position.x + myFloor1.size.width - bird.size.width / 2 + 10 < bird.position.x && bird.position.x < myFloor1.position.x + myFloor1.size.width + holeLength + bird.size.width / 2 - 10){
                 NSLog("im on floor1 edge gotta jump")
                 
                 bird.position.x = myFloor2.position.x + bird.size.width / 2
-                firstJump = true
                 
-            }else if (myFloor2.position.x + myFloor2.size.width < bird.position.x + bird.size.width / 2 && bird.position.x - bird.size.width / 2 < myFloor2.position.x + myFloor2.size.width + myFloor2.size.width / 5  ){
+                // within the death area after floor 2
+            }else if (myFloor2.position.x + myFloor2.size.width - bird.size.width / 2 + 10 < bird.position.x && bird.position.x < myFloor2.position.x + myFloor2.size.width + holeLength + bird.size.width / 2 - 10){
                 NSLog("im on floor2 edge gotta jump")
                 
                 bird.position.x = myFloor1.position.x + bird.size.width / 2
-                firstJump = true
             }
-            
-//            NSLog("1: \(myFloor1.position.x + floorLength - 100), birdPo: \(bird.position.x) ")
-            
-//            if ( myFloor1.position.x + floorLength - 100 < bird.position.x && bird.position.x < myFloor1.position.x + floorLength ){
-//                
-//                bird.position.x = bird.position.x + jumpSpace
-//                
-//                NSLog("hit1")
-//                
-//            }
-            
-            if (myFloor2.position.x < -myFloor2.size.width){
-                jumpSpace = randomBetweenNumbers(firstNum: 80, secondNum: 200)
-                myFloor2.position = CGPoint.init(x: myFloor1.position.x + myFloor1.size.width + jumpSpace, y: myFloor2.position.y)
-                floorLength = randomBetweenNumbers(firstNum: 200, secondNum: 500)
-                NSLog("FloorLength: \(floorLength), JumpSpace: \(jumpSpace)")
-
-                myFloor2.size.width = floorLength
-                
-
-            }
-            
-//            if ( myFloor2.position.x + floorLength - 100 < bird.position.x && bird.position.x < myFloor2.position.x + floorLength ){
-//                
-//                bird.position.x = bird.position.x + jumpSpace
-//                
-//                NSLog("hit2")
-//                
-//            }
         }
     }
     
